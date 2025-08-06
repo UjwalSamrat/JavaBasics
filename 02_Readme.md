5356ba4468d8637b32565239cd3d6dc7ed4474b7
Ujwal Samrat <ujwalsamrat2007@gmail.com>
2025-08-06 19:04:29 +0530

    DataTypes , conditional and loops

---
+++ 02_Readme.md
@@ -0,0 +1,34 @@
+# Java Basics – Day 02
+
+📅 **Date:** August 5, 2025  
+🧑‍💻 **By:** Ujwal Samrat  
+📁 **Topic:** Datatpyes plus Loops and Conditionals
+
+---
+
+## 🔥 What I Learned Today
+- DataTyes in Java
+- Conditional Statements
+  - `if`, `else`, `else if`
+  - Nested conditionals
+- Loops
+  - `for`, `while`, and `do-while`
+- Basic flow control and logic building
+
+---
+
+## 📂 Files Added
+
+| File Name         | Description                              |
+|------------------|------------------------------------------|
+| `Conditionals.java` | Demonstrates if-else and nested conditions |
+| `Loops.java`        | Examples of `for`, `while`, `do-while` loops |
+
+---
+
+## 🚀 How to Run
+
+1. Compile:
+   ```bash
+   javac Loops.java
+   java Loops
--- Datatypes.java
+++ Datatypes.java
@@ -17,5 +17,49 @@ class DataTypes {
         System.out.println("Gravity: " + gravity);
         System.out.println("Initial: " + initial);
         System.out.println("Is coding? " + isCoding);
+        int marks = 85;
+
+        if (marks >= 90) {
+            System.out.println("Excellent");
+        } else if (marks >= 75) {
+            System.out.println("Very Good");
+        } else {
+            System.out.println("Keep Improving");
+        }
+
+
+        int day = 3;
+
+        switch(day) {
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuesday"); break;
+            case 3: System.out.println("Wednesday"); break;
+            default: System.out.println("Invalid day");
+        }
+
+        for (int i = 1; i <= 5; i++) {
+             System.out.println("Count: " + i);
+        }
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Count: " + i);
+            i++;
+        }
+        do {
+            System.out.println("Run at least once");
+            i++;
+        } while (i <= 8);
+
+            for (int j = 1; j <= 10; j++) {
+            if (j == 5) break; // stops at 5
+            System.out.println(i);
+        }
+        for (int l = 1; l <= 5; l++) {
+            if (l == 3) continue; // skips 3
+            System.out.println(i);
+        }
+
+
+
     }
 }
