f57b226cb37b4124c7ab05b62c4e3c028341689e
Ujwal Samrat <ujwalsamrat2007@gmail.com>
2025-08-06 18:46:03 +0530

    Added Java DataTypes using a small program

---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+class DataTypes {
+    public static void main(String[] args) {
+        byte age = 17;
+        short year = 2025;
+        int salary = 100000;
+        long population = 7800000000L;
+        float pi = 3.14f;
+        double gravity = 9.80665;
+        char initial = 'U';
+        boolean isCoding = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Salary: " + salary);
+        System.out.println("Population: " + population);
+        System.out.println("Pi: " + pi);
+        System.out.println("Gravity: " + gravity);
+        System.out.println("Initial: " + initial);
+        System.out.println("Is coding? " + isCoding);
+    }
+}
